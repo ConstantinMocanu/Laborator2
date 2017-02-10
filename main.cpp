9765b256d2ade353137ce052519c32dde5efa118
Constantin Mocanu <constantin.mocanu@toptech.ro>
2017-02-10 21:54:17 +0200

    Exercitiul n

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int h, v; // variabile de intrare
+    double g; // greutatea, valoare calculata
+    cout << "Inaltimea (cm): ";
+    cin >> h; // se citeste inaltimea
+    cout << "Varsta (ani): ";
+    cin >> v; // se citeste varsta
+    g = 50.0 + 0.75*(h-150) + 0.25*(v - 20);
+    cout << "Greutatea ideala (barbat): " << g << " kg" << endl;
+    cout << "Greutatea ideala (femeie): " << 0.9 * g << " kg" << endl;
+    cin.get();
     return 0;
 }
