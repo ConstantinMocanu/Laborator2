b10704d95b782e6188d09b1436d96697346b1404
ConstantinMocanu <constantin.mocanu@toptech.ro>
2017-02-12 16:06:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-    int h, v; // variabile de intrare
-    double g; // greutatea, valoare calculata
+    int h, v;
+    double g;
     cout << "Inaltimea (cm): ";
-    cin >> h; // se citeste inaltimea
+    cin >> h;
     cout << "Varsta (ani): ";
-    cin >> v; // se citeste varsta
+    cin >> v;
     g = 50.0 + 0.75*(h-150) + 0.25*(v - 20);
     cout << "Greutatea ideala (barbat): " << g << " kg" << endl;
     cout << "Greutatea ideala (femeie): " << 0.9 * g << " kg" << endl;
